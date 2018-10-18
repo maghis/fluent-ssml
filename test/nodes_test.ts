8cfc02310eceecd2b08845696a9272542e300d9b
maghis <massimoandreasi@gmail.com>
2018-10-18 09:51:19 -0400

    Updated deps

--- test/nodes_test.ts
+++ test/nodes_test.ts
@@ -1,6 +1,11 @@
 import { test } from "ava";
-import { Emphasis, SayAs } from "../src/nodes";
+import { Emphasis, SayAs, Break } from "../src/nodes";
 import { ssml } from "../src/index";
+
+test("beak attributes", t => {
+    const node = new Break({ time: 100, strength: "strong" });
+    t.deepEqual(node.attributes, { time: "100ms", strength: "strong" });
+});
 
 test("emphasis", t => {
     const node = new Emphasis("strong", ssml("foo"));
